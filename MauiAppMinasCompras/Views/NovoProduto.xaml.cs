00ee307679b3ede4a565c33bd84a143a9525c1d6
Geripoca <geovanahmarins@gmail.com>
2025-03-25 16:00:50 -0300

    Código do Novo Produto

--- MauiAppMinasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinasCompras/Views/NovoProduto.xaml.cs
@@ -6,4 +6,19 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+
 }
